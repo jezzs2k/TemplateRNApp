f966bdaf53e95942c2a6111c8f5057da1ada957a
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2022-01-09 21:50:57 +0700

    config code push for template

--- App.tsx
+++ App.tsx
@@ -1,16 +1,47 @@
 import React, { useEffect } from 'react';
 import { ApplicationNavigator } from 'navigators';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import codePush from 'react-native-code-push';
 
 import { configI18n } from 'common/i18n';
-import { showErrorAlert } from 'common';
+import { showErrorAlert, showSuccessAlert } from 'common';
+
+interface DownloadProgress {
+  totalBytes: number;
+  receivedBytes: number;
+}
 
 const App = () => {
+  function syncStatusChangedCallback(status: codePush.SyncStatus) {
+    //Status
+  }
+
+  function downloadProgressCallback(progress: DownloadProgress) {
+    //Progress
+  }
+
   useEffect(() => {
     configI18n()
       .then()
       .catch(e => {
         showErrorAlert('Config I18n errors');
+      });
+  }, []);
+
+  useEffect(() => {
+    codePush
+      .sync(
+        {
+          installMode: codePush.InstallMode.IMMEDIATE,
+        },
+        syncStatusChangedCallback,
+        downloadProgressCallback,
+      )
+      .then((status: codePush.SyncStatus) => {
+        showSuccessAlert('Sync CodePush', `${status}`);
+      })
+      .catch(() => {
+        showErrorAlert('Sync codepush errors');
       });
   }, []);
   return (
